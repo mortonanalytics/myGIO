8ebd805f9e374ac3bb3c59fd5f9daa76f0c44a6b
Ryan Morton <mortonanalytics@gmail.com>
2018-08-19 16:52:57 -0600

    Initial commit

---
+++ R/myGIO.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+myGIO <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'myGIO',
+    x,
+    width = width,
+    height = height,
+    package = 'myGIO',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for myGIO
+#'
+#' Output and render functions for using myGIO within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a myGIO
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name myGIO-shiny
+#'
+#' @export
+myGIOOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'myGIO', width, height, package = 'myGIO')
+}
+
+#' @rdname myGIO-shiny
+#' @export
+renderMyGIO <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, myGIOOutput, env, quoted = TRUE)
+}
