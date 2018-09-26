5d69ee71037455087ab52a45279ee4ab15170dbd
Ryan Morton <mortonanalytics@gmail.com>
2018-09-26 16:18:17 -0600

    add tests for base layer function

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(myGIO)
+
+test_check("myGIO")
---
+++ tests/testthat/test_layers.R
@@ -0,0 +1,18 @@
+# test IO layer
+context("layer_functions")
+
+df <- data.frame(zipcode = c("59801", "59802", "59803", "59804","59805", "59606", "59601", "59602"),
+                 values = c(100,200,300,400,400,300,400,200),
+                 stringsAsFactors = FALSE)
+
+
+test_object <- myGIO::myGIO() %>%
+  myGIO::addBase(base = "zipChloropleth",
+                 data = df,
+                 mapping = list(dataKey = "zipcode",
+                                dataValue = "values",
+                                mapKey = "zip"))
+
+testthat::test_that("add layer creates a list of one", {
+  expect_output(str(test_object$x$layers), "List of 1")
+})
