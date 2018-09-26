14f7f8438196bb8c0f14dfebf861f1bb70ae0364
Ryan Morton <mortonanalytics@gmail.com>
2018-09-26 16:11:03 -0600

    added zip chloropleth base layer
    base layer zooms to bounding box min/max so as to focus on the data available; currently only fills polygons

--- R/addBase.R
+++ R/addBase.R
@@ -4,18 +4,22 @@
 #'
 #' @param myGIo the widget object
 #' @param base a string indicating the base map to be used
+#' @param data is a dataframe to be mapped tot he base layer
+#' @param mapping is a list object mapping the data to the map features; requires a data key and map key
 #'
 #' @return myGIO object with base layer attached
 #' @export
-addBase <- function(myGIO, base){
+addBase <- function(myGIO, base, data = NULL, mapping = NULL){
 
   #assert layer type
   stopifnot(is.character(base))
-  stopifnot(base %in% c("world", "us"))
+  stopifnot(base %in% c("world", "us", "zipChloropleth"))
 
   layer <- list(
     type = "base",
-    map = base
+    map = base,
+    data = data,
+    mapping = mapping
   )
 
   ##put the layers together
