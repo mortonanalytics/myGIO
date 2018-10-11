5cc2518e5dfbd6dfa9e3b95af29fc5b89300d8d9
Ryan Morton <mortonanalytics@gmail.com>
2018-10-11 16:04:47 -0600

    account for sidebar on tooltip

--- inst/htmlwidgets/myGIO/myGIOapi.js
+++ inst/htmlwidgets/myGIO/myGIOapi.js
@@ -385,6 +385,8 @@ myGIOmap.prototype.addZipChloropleth = function(ly, chartElement){
 		var object = d3.select(this).data()[0];
 		var objectBox = path.bounds(object);
 		var objectData = object.properties.values[0];
+		var sidebar = d3.select('#sidebarCollapsed');
+		console.log(sidebar);
 		var nameFormat = that.options.nameFormat != "text" ? d3.format(that.options.nameFormat ? that.options.nameFormat : "d") : function(x) {return x;} ;
 		var valueFormat = d3.format(that.options.valueFormat ? that.options.valueFormat : "d");
 		var toolTipFormat = d3.format(that.options.toolTipFormat ? that.options.toolTipFormat : "d");
